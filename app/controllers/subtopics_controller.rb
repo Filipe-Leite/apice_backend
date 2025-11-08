3dd356dbaac448e1c473798155daac78eb7b3bb1
Filipe-leite <filipedasilva94.1@gmail.com>
2025-11-08 12:57:15 -0300

    solving bug in second level navigation

--- app/controllers/subtopics_controller.rb
+++ app/controllers/subtopics_controller.rb
@@ -3,12 +3,17 @@ class SubtopicsController < ApplicationController
   def show
     @subtopics = Subtopic.all
     
-    if params[:letter].present?
+    if params[:discipline].present?
+      @subtopics = @subtopics.joins(subtopics_topics: { topic: :topics_disciplines })
+                            .where(topics_disciplines: { discipline_id: params[:discipline] })
+    end
+    
+    if params[:letter].present? && params[:discipline].blank?
       @subtopics = @subtopics.where(
         Subtopic.arel_table[:name].matches("#{params[:letter]}%")
       )
     end
-    
+
     @subtopics = @subtopics.paginate(page: params[:page], per_page: 10)
 
     render json: @subtopics, status: :ok
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,18 +3,30 @@ class TopicsController < ApplicationController
   def show
 
     @topics = Topic.all
+
+    p "afafadf >>>>>>>> #{params}"
+    p "afafadf >>>>>>>> #{params}"
+    p "afafadf >>>>>>>> #{params}"
+    p "afafadf >>>>>>>> #{params}"
+    p "afafadf >>>>>>>> #{params}"
+    p "afafadf >>>>>>>> #{params}"
+    p "afafadf >>>>>>>> #{params}"
+    p "afafadf >>>>>>>> #{params}"
+    p "afafadf >>>>>>>> #{params}"
+    p "afafadf >>>>>>>> #{params}"
+    p "afafadf >>>>>>>> #{params}"
     
-    if params[:letter].present?
+    if params[:discipline].present? 
+      @topics = @topics.joins(:topics_disciplines).where(topics_disciplines: { discipline_id: params[:discipline] })
+    end
+
+    if params[:letter].present? && params[:discipline].blank
       @topics = @topics.where(
         Topic.arel_table[:name].matches("#{params[:letter]}%")
       )
     end
 
-    if params[:discipline].present?
-      @topics = @topics.joins(:topics_disciplines).where(topics_disciplines: { discipline_id: params[:discipline] })
-    end
-
-    @topics = @topics.paginate(page: params[:page], per_page: 200)
+    @topics = @topics.paginate(page: params[:page], per_page: 10)
 
     render json: @topics, status: :ok
   end
